f64e29a85e14a8cece9f737e832adfb6ad571e35
haoyu qin <lxliuyuan@hotmail.com>
2020-05-09 09:36:45 +0900

    Change the class

--- MainProgram.cs
+++ MainProgram.cs
@@ -17,17 +17,12 @@ namespace NsetOutputFC
            
             int weeklyRange = Convert.ToInt32(ConfigurationManager.AppSettings["weeklyRange"]);
             int monthlyRange = Convert.ToInt32(ConfigurationManager.AppSettings["monthlyRange"]);
-     
-            DataTable dtMyFcWeekly = new ConnDB(0).Get_Pur_FcstInqList(weeklyRange).Tables[0];
-            DataTable dtMyFcMonthly = new ConnDB(1).Get_Pur_FcstInqList(monthlyRange).Tables[0];
-            SplitDt weekSplit = new SplitDt(weeklyRange,0);
-            SplitDt monthSplit = new SplitDt(monthlyRange, 1);
+            int weeklyType = 0;
+            int monthlyType = 1;
+        
 
-            DataSet weekDataSet = weekSplit.Change(dtMyFcWeekly);
-            DataSet monthDataSet = monthSplit.Change(dtMyFcMonthly);
-
-            CsvCreater weeklyCsv = new CsvCreater(weekDataSet,"Weekly_");
-            CsvCreater monthlyCsv = new CsvCreater(monthDataSet,"Monthly_");
+            CsvCreater weeklyCsv = new CsvCreater("Weekly_",weeklyType,weeklyRange);
+            CsvCreater monthlyCsv = new CsvCreater("Monthly_",monthlyType,monthlyRange);
             weeklyCsv.OutPutCsv();
             monthlyCsv.OutPutCsv();
         
@@ -50,56 +45,65 @@ namespace NsetOutputFC
         }
         public DataSet Change(DataTable dt)
         {
-            DataSet ds = new DataSet();
-            for(int i=0;i<dt.Rows.Count;)
+            try
             {
-                
-                DataTable tempDt = new DataTable();
-                tempDt.TableName = dt.Rows[i]["VENDOR_CD"].ToString()+"_"+ dt.Rows[i]["VENDOR_DESC"].ToString();
-                tempDt.Columns.Add("Stock_Id");
-                tempDt.Columns.Add("Stock_Name");
-                for(int k = 0; k < _range;k++)
+                DataSet ds = new DataSet();
+                for (int i = 0; i < dt.Rows.Count;)
                 {
-                    if (_dataType == 0)
+
+                    DataTable tempDt = new DataTable();
+                    tempDt.TableName = dt.Rows[i]["VENDOR_CD"].ToString() + "_" + dt.Rows[i]["VENDOR_DESC"].ToString();
+                    tempDt.Columns.Add("Stock_Id");
+                    tempDt.Columns.Add("Stock_Name");
+                    for (int k = 0; k < _range; k++)
                     {
-                        DateTime dateTime = DateTime.Now.AddDays(k * 7);
-                        DateTime startWeek = dateTime.AddDays(1 - Convert.ToInt32(dateTime.DayOfWeek.ToString("d")));
-                        DateTime endWeek = startWeek.AddDays(6);
-                        string colName = startWeek.ToString("MMdd") +"-"+ endWeek.ToString("MMdd");
-                        tempDt.Columns.Add(colName);
+                        if (_dataType == 0)
+                        {
+                            DateTime dateTime = DateTime.Now.AddDays(k * 7);
+                            DateTime startWeek = dateTime.AddDays(1 - Convert.ToInt32(dateTime.DayOfWeek.ToString("d")));
+                            DateTime endWeek = startWeek.AddDays(6);
+                            string colName = startWeek.ToString("MMdd") + "-" + endWeek.ToString("MMdd");
+                            tempDt.Columns.Add(colName);
+                        }
+                        else
+                        {
+                            string colName = DateTime.Now.AddMonths(k).ToString("yyyyMM");
+                            tempDt.Columns.Add(colName);
+                        }
+
                     }
-                    else
-                    {
-                        string colName=DateTime.Now.AddMonths(k).ToString("yyyyMM");
-                        tempDt.Columns.Add(colName);
-                    }
-                    
-                }
-                int j = 0;
-                do
-                {
-                    
-                    tempDt.Rows.Add();
+                    int j = 0;
                     do
                     {
-                        string DateNO = dt.Rows[i]["DATE_NO"].ToString();
-                        
-                        tempDt.Rows[j]["Stock_Id"] = dt.Rows[i]["ITEM_NO"].ToString();
-                        tempDt.Rows[j]["Stock_Name"] = dt.Rows[i]["ITEM_DESC"].ToString();
-                        tempDt.Rows[j][DateNO] = dt.Rows[i]["PO_QTY"].ToString();
-                        i++;
-                        
+
+                        tempDt.Rows.Add();
+                        do
+                        {
+                            string DateNO = dt.Rows[i]["DATE_NO"].ToString();
+
+                            tempDt.Rows[j]["Stock_Id"] = dt.Rows[i]["ITEM_NO"].ToString();
+                            tempDt.Rows[j]["Stock_Name"] = dt.Rows[i]["ITEM_DESC"].ToString();
+                            tempDt.Rows[j][DateNO] = dt.Rows[i]["PO_QTY"].ToString();
+                            i++;
+
+
+                        }
+                        while (i < dt.Rows.Count && dt.Rows[i]["VENDOR_CD"].ToString() == dt.Rows[i - 1]["VENDOR_CD"].ToString() && dt.Rows[i]["ITEM_NO"].ToString() == dt.Rows[i - 1]["ITEM_NO"].ToString());
+                        j++;
+
 
                     }
-                    while (i < dt.Rows.Count && dt.Rows[i]["VENDOR_CD"].ToString() == dt.Rows[i - 1]["VENDOR_CD"].ToString()&& dt.Rows[i]["ITEM_NO"].ToString() == dt.Rows[i - 1]["ITEM_NO"].ToString());
-                    j++;
-                    
-
+                    while (i < dt.Rows.Count && dt.Rows[i]["VENDOR_CD"].ToString() == dt.Rows[i - 1]["VENDOR_CD"].ToString());
+                    ds.Tables.Add(tempDt);
                 }
-                while (i < dt.Rows.Count && dt.Rows[i]["VENDOR_CD"].ToString()== dt.Rows[i-1]["VENDOR_CD"].ToString());
-                ds.Tables.Add(tempDt);
+                return ds;
+
             }
-            return ds;
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+          
         }
         
     }
@@ -108,34 +112,57 @@ namespace NsetOutputFC
     /// </summary>
     public class CsvCreater
     {
-        DataSet ds = new DataSet();
+        
+
+        int dataType;
+        int range;
         string type;
         string allLog;
         string logFolderPath = ConfigurationManager.AppSettings["logFolderPath"];
         string targetFolderPath = ConfigurationManager.AppSettings["targetFolderPath"];
 
-        public CsvCreater(DataSet Ds,string Type)
+        public CsvCreater(string Type,int DataType,int Range)
         {
-            ds = Ds;
+          
             type = Type;
+            dataType = DataType;
+            range = Range;
         }
 
         public void OutPutCsv()
         {
-            foreach(DataTable dt in ds.Tables)
+            try
             {
-                string strOutPutFolderPath =targetFolderPath+@"\"+ dt.TableName.Split('_')[0];
-                string strOutPutCSVPath = strOutPutFolderPath +@"\" +type+ dt.TableName.Split('_')[0] + "_"+ DateTime.Now.ToString("yyyyMMdd")+@".csv";
-                if (!Directory.Exists(strOutPutFolderPath))
+                DataTable dtBefore = new ConnDB(dataType).Get_Pur_FcstInqList(range).Tables[0];
+                SplitDt Split = new SplitDt(range, dataType);
+                DataSet ds = Split.Change(dtBefore);
+
+                foreach (DataTable dt in ds.Tables)
                 {
-                    Directory.CreateDirectory(strOutPutFolderPath);
-                    allLog += type+dt.TableName.Split('_')[0] + "  Folder was created.\n";     
+                    string strOutPutFolderPath = targetFolderPath + @"\" + dt.TableName.Split('_')[0];
+                    string strOutPutCSVPath = strOutPutFolderPath + @"\" + type + dt.TableName.Split('_')[0] + "_" + DateTime.Now.ToString("yyyyMMdd") + @".csv";
+                    if (!Directory.Exists(strOutPutFolderPath))
+                    {
+                        Directory.CreateDirectory(strOutPutFolderPath);
+                        allLog += type + dt.TableName.Split('_')[0] + "  Folder was created.\n";
+                    }
+                    SaveCsv(dt, strOutPutCSVPath);
+                    allLog += type + dt.TableName.Split('_')[0] + "_" + DateTime.Now.ToString("yyyyMMdd") + "  Csv was created.\n";
+
                 }
-                SaveCsv(dt, strOutPutCSVPath);
-                allLog +=type+dt.TableName.Split('_')[0] + "_" + DateTime.Now.ToString("yyyyMMdd")+ "  Csv was created.\n";
 
             }
-            File.AppendAllText(logFolderPath + @"\" + DateTime.Now.ToString("yyyy-MM-dd H-mm-ss") + "log.txt", "\r\n" + allLog);
+            catch(Exception ex)
+            {
+                allLog += ex.Message;
+               
+            }
+            finally
+            {
+                File.AppendAllText(logFolderPath + @"\" + DateTime.Now.ToString("yyyy-MM-dd H-mm-ss") + "log.txt", "\r\n" + allLog);
+            }
+           
+            
         }
         private void SaveCsv(DataTable dt, string filePath)
         {
@@ -145,14 +172,17 @@ namespace NsetOutputFC
             {
                 fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
                 sw = new StreamWriter(fs, Encoding.Default);
-                var data = string.Empty;
-                sw.WriteLine("," + "NIDEC SHIBAURA ELECTRONICS(THAILAND) CO,LTD.");
-                sw.WriteLine("," + "144 / 4 Moo.5 Tivanon Road. Bangkadi Industrial Park");
-                sw.WriteLine("," + "Tumbol Bangkadi. Amphur Muang. Patumthani 12000");
-                sw.WriteLine("," + "Tel: (662) 831 - 9000 Fax: (662) 963 - 8235 . 963 - 8302");              
+                var data = string.Empty;       
+                sw.WriteLine("," + "\"NIDEC SHIBAURA ELECTRONICS(THAILAND) CO.,LTD.\"");
+                sw.WriteLine("," + "\"144 / 4 Moo.5 Tivanon Road,Bangkadi Industrial Park,\"");
+                sw.WriteLine("," + "\"Tumbol Bangkadi,Amphur Muang,Patumthani 12000\"");
+                sw.WriteLine("," + "\"Tel: (662) 831 - 9000 Fax: (662) 963 - 8235 , 963 - 8302\"");
+                sw.WriteLine(" ");
                 sw.WriteLine(","+"ORDER FORECAST");
+                sw.WriteLine(" ");
                 sw.WriteLine("To:,"+ dt.TableName.Split('_')[1]);
-                sw.WriteLine(",,,,Date"+","+DateTime.Now.ToString("yy-MM-dd"));
+                sw.WriteLine("Date"+","+DateTime.Now.ToString("yy-MM-dd"));
+                sw.WriteLine(" ");
                 //Column Name
                 for (var i = 0; i < dt.Columns.Count; i++)
                 {
